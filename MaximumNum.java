e3547b89cb882020308f1fd0b5cba78bb85a6f71
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-05-04 22:37:53 +0530

    ADD:Integer find the maximum value

--- MaximumNum.java
+++ MaximumNum.java
@@ -1,7 +1,20 @@
-class MaximumNum
-{
-public static void main(String args[])
-{
-System.out.println("Welcome to Generic problem");
-}
-}
+package com.bridgelabz.generic_problem;
+
+public class MaximumNum {
+	
+	public static int getIntMaxNum(Integer x,Integer y,Integer z)
+	{
+		int maxValue=x;
+		if(y.compareTo(maxValue)>0)
+		maxValue=y;
+		if(z.compareTo(maxValue)>0)
+		maxValue=z;
+		return maxValue;
+		
+	}	
+		public static void main(String args[])
+		{
+			System.out.println("welcome to maximum number problem using generic method ");
+		}
+	}
+
