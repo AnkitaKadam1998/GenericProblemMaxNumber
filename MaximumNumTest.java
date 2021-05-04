7178dd599721a39dfcbb6ecf282f11b7e73e0b58
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-05-05 00:24:30 +0530

    ADD:Test cases for String

--- MaximumNumTest.java
+++ MaximumNumTest.java
@@ -8,19 +8,23 @@ public class MaximumNumTest
 	
 	@Test
     public void MaxNumber_FirstPosition_ReturnsTrue_IfSameAsExpected(){
-        Float maxValue=MaximumNum.getFloatMaxNum(10.0f,12.34f,11.10f);
-        assertEquals(12.34,maxValue,0);
+        String maxValue=MaximumNum.getStringMaxNum("Apple","peach","banana");
+        assertEquals("peach",maxValue);
+        
     }
-	
+
 	@Test
-    public void MaxNumber_SecondPosition_ReturnsTrue_IfSameAsExpected(){
-        Float maxValue=MaximumNum.getFloatMaxNum(21.6f,22.7f,23.8f);
-        assertEquals(23.8f,maxValue,0);
-    }
+	public void MaxNumber_SecondPosition_ReturnsTrue_IfSameAsExpected(){
+		String maxValue=MaximumNum.getStringMaxNum("banana","peach","apple");
+		assertEquals("peach",maxValue);
+    
+}
 	
 	@Test
     public void MaxNumber_ThirdPosition_ReturnsTrue_IfSameAsExpected(){
-        Float maxValue=MaximumNum.getFloatMaxNum(13.5,22.7,26.0);
-        assertEquals(26.0,maxValue,0);
+		String maxValue=MaximumNum.getStringMaxNum("Apple","banana","peach");
+		assertEquals("peach",maxValue);
     }
 }
+
+
