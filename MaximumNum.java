47f35d6e924d1d8d2472b670e60315d668e30493
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-05-05 16:33:41 +0530

    ADD:Refactor all the 3 to

--- MaximumNum.java
+++ MaximumNum.java
@@ -1,15 +1,16 @@
 package com.bridgelabz.generic_problem;
 
-public class MaximumNum {
-	
-	public static String getStringMaxNum(String x,String y,String z)
+public class MaximumNum 
 	{
-		String maxValue=x;
-		if(y.compareTo(maxValue)>0)
-		maxValue=y;
-		if(z.compareTo(maxValue)>0)
-		maxValue=z;
-		return maxValue;	
+		public static <E extends Comparable<E>> E printMaxValue(E a,E b,E c)
+		{
+		E maxValue= a;
+		if(b.compareTo(maxValue)>0)
+		maxValue=b;
+		if(c.compareTo(maxValue)>0)
+		maxValue=c;
+		return maxValue;
+		}	
 	}
-	}
+		
 
